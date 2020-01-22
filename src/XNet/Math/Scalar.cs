6310e506e25ca620de67af21a0565f476c0208b7
Nirex <Nirex.0@gmail.com>
2020-01-22 04:20:59 +0330

    Implement: Scalar class

--- src/XNet/Math/Scalar.cs
+++ src/XNet/Math/Scalar.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace XNet.Math
+﻿namespace XNet.Math
 {
+    /// <summary>
+    /// Simple Scalar class used to store floating point values.
+    /// Note: This class can be set to be constant upon instantiation.
+    /// </summary>
     public class Scalar
     {
+        public Scalar(float val = 0, bool isConst = false)
+        {
+            Value = val;
+            IsConst = isConst;
+        }
+
+        public float Value
+        {
+            get
+            {
+                return Value;
+            }
+            set
+            {
+                if (!IsConst)
+                {
+                    Value = value;
+                }
+            }
+        }
+        public bool IsConst { get; private set; }
     }
 }
