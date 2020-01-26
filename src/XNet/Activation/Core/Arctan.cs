b2780a6f20949094a01ae10eb11ee111bd75f735
Nirex0 <Nirex.0@gmail.com>
2020-01-26 15:37:52 +0330

    Update Arctan.cs

--- src/XNet/Activation/Core/Arctan.cs
+++ src/XNet/Activation/Core/Arctan.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using XNet.Math;
 
 namespace XNet.Activation.Core
 {
-    class Arctan
+    public sealed class Arctan : Utility.Activation
     {
+        public override Matrix Forward(Matrix input)
+        {
+            return Matrix.Map(input, Activate);
+        }
+
+        public override Matrix Backward(Matrix input)
+        {
+            return Matrix.Map(input, Derivative);
+        }
+
+        public override double Activate(double input)
+        {
+            return System.Math.Atan(input);
+        }
+
+        public override double Derivative(double input)
+        {
+            return 1 / (1 + System.Math.Pow(input, 2));
+        }
+
+        public override Utility.EActivationType Type()
+        {
+            return Utility.EActivationType.Arctan;
+        }
+
+        public override string ToString()
+        {
+            return Type().ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
