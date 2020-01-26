420a6d62d6525eda6215eabb36878e5f83cff684
Nirex0 <Nirex.0@gmail.com>
2020-01-26 16:24:24 +0330

    Implement: LReLU Activation class

--- src/XNet/Activation/Core/LReLU.cs
+++ src/XNet/Activation/Core/LReLU.cs
@@ -1,12 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using XNet.Math;
 
 namespace XNet.Activation.Core
 {
-    class LReLU
+    public sealed class LReLU : Utility.Activation
     {
+        public double Alpha { get; set; }
+
+        public LReLU(double alpha)
+        {
+            Alpha = alpha;
+        }
+        public override Matrix Forward(Matrix input)
+        {
+            return Matrix.Map(input, Activate);
+        }
+
+        public override Matrix Backward(Matrix input)
+        {
+            return Matrix.Map(input, Derivative);
+        }
+
+        public override double Activate(double input)
+        {
+            return System.Math.Max(input, Alpha);
+        }
+
+        public override double Derivative(double input)
+        {
+            if (input > 0)
+            {
+                return 1;
+            }
+            return Alpha;
+        }
+
+        public override Utility.EActivationType Type()
+        {
+            return Utility.EActivationType.LReLU;
+        }
+
+        public override string ToString()
+        {
+            return Type().ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
