7afd40d01b6b507706afbb5d40a3e19d996e5352
Nirex0 <Nirex.0@gmail.com>
2020-01-26 16:24:39 +0330

    Implement: ReLU Activation class

--- src/XNet/Activation/Core/ReLU.cs
+++ src/XNet/Activation/Core/ReLU.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using XNet.Math;
 
 namespace XNet.Activation.Core
 {
-    class ReLU
+    public sealed class ReLUS : Utility.Activation
     {
+        public override Matrix Forward(Matrix input)
+        {
+            return Matrix.Map(input, Activate);
+        }
+
+        public override Matrix Backward(Matrix input)
+        {
+            return Matrix.Map(input, Derivative);
+        }
+
+        public override double Activate(double input)
+        {
+            return System.Math.Max(input, 0);
+        }
+
+        public override double Derivative(double input)
+        {
+            if (input > 0)
+            {
+                return 1;
+            }
+            return 0;
+        }
+
+        public override Utility.EActivationType Type()
+        {
+            return Utility.EActivationType.ReLU;
+        }
+
+        public override string ToString()
+        {
+            return Type().ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
