83e0d20a462d44ae97a5d0282888b15bb95e112c
Nirex0 <Nirex.0@gmail.com>
2020-01-26 16:25:18 +0330

    Implement: Tanh Activation class

--- src/XNet/Activation/Core/Tanh.cs
+++ src/XNet/Activation/Core/Tanh.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using XNet.Math;
 
 namespace XNet.Activation.Core
 {
-    class Tanh
+    public sealed class Tanh : Utility.Activation
     {
+        public override Matrix Forward(Matrix input)
+        {
+            return Matrix.Map(input, Activate);
+        }
+
+        public override Matrix Backward(Matrix input)
+        {
+            return Matrix.Map(input, Derivative);
+        }
+
+        public override double Activate(double input)
+        {
+            return (System.Math.Exp(input) - System.Math.Exp(-input)) / (System.Math.Exp(input) + System.Math.Exp(-input));
+        }
+
+        public override double Derivative(double x)
+        {
+            return 1 - System.Math.Pow((System.Math.Exp(x) - System.Math.Exp(-x)) / (System.Math.Exp(x) + System.Math.Exp(-x)), 2);
+        }
+
+        public override Utility.EActivationType Type()
+        {
+            return Utility.EActivationType.Arctan;
+        }
+
+        public override string ToString()
+        {
+            return Type().ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
