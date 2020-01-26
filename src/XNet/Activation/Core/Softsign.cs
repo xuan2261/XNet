b7014e94595c684da43a3676fc2d8d3480ace98b
Nirex0 <Nirex.0@gmail.com>
2020-01-26 16:25:12 +0330

    Implement: Softsign Activation class

--- src/XNet/Activation/Core/Softsign.cs
+++ src/XNet/Activation/Core/Softsign.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using XNet.Math;
 
 namespace XNet.Activation.Core
 {
-    class Softsign
+    public sealed class Softsign : Utility.Activation
     {
+        public override Matrix Forward(Matrix input)
+        {
+            return Matrix.Map(input, Activate);
+        }
+
+        public override Matrix Backward(Matrix input)
+        {
+            return Matrix.Map(input, Derivative);
+        }
+
+        public override double Activate(double input)
+        {
+            return input / (1 + System.Math.Abs(input));
+        }
+
+        public override double Derivative(double input)
+        {
+            return input / System.Math.Pow((1 + System.Math.Abs(input)), 2);
+        }
+
+        public override Utility.EActivationType Type()
+        {
+            return Utility.EActivationType.Softsign;
+        }
+
+        public override string ToString()
+        {
+            return Type().ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
