73a43139f2e5bbcd96e2bbea9eb25122e6c94f81
Nirex0 <Nirex.0@gmail.com>
2020-01-26 16:24:17 +0330

    Implement: Logit Activation class

--- src/XNet/Activation/Core/Logit.cs
+++ src/XNet/Activation/Core/Logit.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using XNet.Math;
 
 namespace XNet.Activation.Core
 {
-    class Logit
+    public sealed class Logit : Utility.Activation
     {
+        public override Matrix Forward(Matrix input)
+        {
+            return Matrix.Map(input, Activate);
+        }
+
+        public override Matrix Backward(Matrix input)
+        {
+            return Matrix.Map(input, Derivative);
+        }
+
+        public override double Activate(double input)
+        {
+            return System.Math.Log(input / (1 - input));
+        }
+
+        public override double Derivative(double input)
+        {
+            return (-1 / System.Math.Pow(input, 2)) - (1 / (System.Math.Pow((1 - input), 2)));
+        }
+
+        public override Utility.EActivationType Type()
+        {
+            return Utility.EActivationType.Logit;
+        }
+
+        public override string ToString()
+        {
+            return Type().ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
