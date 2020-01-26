96996bede43f22425b06da907f223858a132e396
Nirex0 <Nirex.0@gmail.com>
2020-01-26 15:37:42 +0330

    Update ActivationSettings.cs

--- src/XNet/Activation/Utility/ActivationSettings.cs
+++ src/XNet/Activation/Utility/ActivationSettings.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace XNet.Utility.Activation
+﻿namespace XNet.Activation.Utility
 {
     public abstract class ActivationSettings
     {
