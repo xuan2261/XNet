1ebac3f4199ab8794f013c682ea94b1aeeef28b9
Nirex <Nirex.0@gmail.com>
2020-01-26 21:29:30 +0330

    Update AdaDelta.cs

--- src/XNet/Optimizer/Core/AdaDelta.cs
+++ src/XNet/Optimizer/Core/AdaDelta.cs
@@ -3,39 +3,8 @@ using XNet.Optimizer.Utility;
 
 namespace XNet.Optimizer.Core
 {
-    public sealed class AdaDelta : Utility.Optimizer
+    public sealed class AdaDelta
     {
-        public double Alpha { get; set; }
 
-        public AdaDelta(double alpha)
-        {
-            Alpha = alpha;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return base.Equals(obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
-        }
-
-        public override Matrix OptimizeMatrix(Matrix X, Matrix dJdX)
-        {
-            Matrix deltaX = (Alpha * (Matrix.Transpose(X) * dJdX));
-            return deltaX;
-        }
-
-        public override string ToString()
-        {
-            return Type().ToString();
-        }
-
-        public override EOptimizerType Type()
-        {
-            return EOptimizerType.GradientDescent;
-        }
     }
 }
