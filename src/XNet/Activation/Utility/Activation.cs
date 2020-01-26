c72c69559efb1b17ba36b7a3df403b581f21c4bc
Nirex0 <Nirex.0@gmail.com>
2020-01-26 15:37:49 +0330

    Update Activation.cs

--- src/XNet/Activation/Utility/Activation.cs
+++ src/XNet/Activation/Utility/Activation.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using XNet.Math;
 
-namespace XNet.Utility.Activation
+namespace XNet.Activation.Utility
 {
     public abstract class Activation
     {
+        // Forward Method -> 
+        public abstract Matrix Forward(Matrix input);
+
+        // Backward Method <- (Derivative)
+        public abstract Matrix Backward(Matrix input);
+
+        // Activate Method for a single value
+        public abstract double Activate(double input);
+
+        // Derivative Method for a single value
+        public abstract double Derivative(double input);
+
+        // Gets the type of the Activation Function Class
+        public abstract Utility.EActivationType Type();
+        
     }
 }
