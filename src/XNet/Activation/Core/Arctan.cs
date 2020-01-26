0699ece4028034c955baa0a76d2406d50507f8dc
Nirex <Nirex.0@gmail.com>
2020-01-26 21:28:51 +0330

    Add: Copyright Notice to Arctan.cs

--- src/XNet/Activation/Core/Arctan.cs
+++ src/XNet/Activation/Core/Arctan.cs
@@ -1,47 +1,25 @@
-﻿using XNet.Math;
+﻿// Copyright © 2020 Aryan Mousavi All Rights Reserved.
+
+using XNet.Math;
 
 namespace XNet.Activation.Core
 {
     public sealed class Arctan : Utility.Activation
     {
-        public override Matrix Forward(Matrix input)
-        {
-            return Matrix.Map(input, Activate);
-        }
+        public override Matrix Forward(Matrix input) => Matrix.Map(input, Activate);
 
-        public override Matrix Backward(Matrix input)
-        {
-            return Matrix.Map(input, Derivative);
-        }
+        public override Matrix Backward(Matrix input) => Matrix.Map(input, Derivative);
 
-        public override double Activate(double input)
-        {
-            return System.Math.Atan(input);
-        }
+        public override double Activate(double input) => System.Math.Atan(input);
+        
+        public override double Derivative(double input) => 1 / (1 + System.Math.Pow(input, 2));
 
-        public override double Derivative(double input)
-        {
-            return 1 / (1 + System.Math.Pow(input, 2));
-        }
+        public override Utility.EActivationType Type() => Utility.EActivationType.Arctan;
 
-        public override Utility.EActivationType Type()
-        {
-            return Utility.EActivationType.Arctan;
-        }
+        public override string ToString() => Type().ToString();
 
-        public override string ToString()
-        {
-            return Type().ToString();
-        }
+        public override bool Equals(object obj) => base.Equals(obj);
 
-        public override bool Equals(object obj)
-        {
-            return base.Equals(obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
-        }
+        public override int GetHashCode() => base.GetHashCode();
     }
 }
