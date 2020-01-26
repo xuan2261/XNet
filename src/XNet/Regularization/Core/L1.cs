bf32a794138406f9539134f7bace3b74553d9a2a
Nirex0 <Nirex.0@gmail.com>
2020-01-26 17:57:07 +0330

    Implement: L1 Regularization

--- src/XNet/Regularization/Core/L1.cs
+++ src/XNet/Regularization/Core/L1.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using XNet.Math;
+using XNet.Regularization.Utility;
 
 namespace XNet.Regularization.Core
 {
-    class L1
+    public sealed class L1 : Utility.Regularization
     {
+        public L1(double lambda) : base(lambda) { }
+
+        public override bool Equals(object obj) => base.Equals(obj);
+
+        public override int GetHashCode() => base.GetHashCode();
+
+        public override double CalculateNorm(Matrix X) => (Matrix.AbsoluteNorm(X) * Lambda);
+
+        public override string ToString() => Type().ToString();
+
+        public override ERegularizationType Type() => ERegularizationType.L1;
     }
 }
