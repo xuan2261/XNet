7676e13d54215ffb338088c096090380550a7a9f
Nirex0 <Nirex.0@gmail.com>
2020-01-26 16:24:35 +0330

    Implement: Mish Activation class

---
+++ src/XNet/Activation/Core/Mish.cs
@@ -0,0 +1,50 @@
+﻿using XNet.Math;
+
+namespace XNet.Activation.Core
+{
+    public sealed class Mish : Utility.Activation
+    {
+        public override Matrix Forward(Matrix input)
+        {
+            return Matrix.Map(input, Activate);
+        }
+
+        public override Matrix Backward(Matrix input)
+        {
+            return Matrix.Map(input, Derivative);
+        }
+
+        public override double Activate(double input)
+        {
+            return input * System.Math.Tanh(System.Math.Log(1 + System.Math.Exp(input)));
+        }
+
+        public override double Derivative(double input)
+        {
+            double epsilon = 2 * System.Math.Exp(input) + System.Math.Exp(2 * input) + 2;
+            double omega = 4 * (input + 1) + 4 * System.Math.Exp(2 * input) + System.Math.Exp(3 * input) + System.Math.Exp(input) * (4 * input + 6);
+
+            return ((System.Math.Exp(input) * omega) / System.Math.Pow(epsilon, 2));
+        }
+
+        public override Utility.EActivationType Type()
+        {
+            return Utility.EActivationType.Mish;
+        }
+
+        public override string ToString()
+        {
+            return Type().ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
