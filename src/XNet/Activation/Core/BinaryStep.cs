1acb87aaf98f6bc087167df6bc5b1915f0ce6229
Nirex0 <Nirex.0@gmail.com>
2020-01-26 16:23:40 +0330

    Implement: Binary Step Activation class

--- src/XNet/Activation/Core/BinaryStep.cs
+++ src/XNet/Activation/Core/BinaryStep.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using XNet.Math;
 
 namespace XNet.Activation.Core
 {
-    class BinaryStep
+    public sealed class BinaryStep : Utility.Activation
     {
+        public override Matrix Forward(Matrix input)
+        {
+            return Matrix.Map(input, Activate);
+        }
+
+        public override Matrix Backward(Matrix input)
+        {
+            return Matrix.Map(input, Derivative);
+        }
+
+        public override double Activate(double input)
+        {
+            if (input < 0)
+            {
+                return 0;
+            }
+            return 1;
+        }
+
+        public override double Derivative(double input)
+        {
+            return 0;
+        }
+
+        public override Utility.EActivationType Type()
+        {
+            return Utility.EActivationType.BinaryStep;
+        }
+
+        public override string ToString()
+        {
+            return Type().ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
