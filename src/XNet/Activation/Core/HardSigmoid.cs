6f734abaf337270ca9a10b240005a64e854c7a2e
Nirex0 <Nirex.0@gmail.com>
2020-01-26 16:24:05 +0330

    Implement: Hard Sigmoid Activation class

--- src/XNet/Activation/Core/HardSigmoid.cs
+++ src/XNet/Activation/Core/HardSigmoid.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using XNet.Math;
 
 namespace XNet.Activation.Core
 {
-    class HardSigmoid
+    public sealed class HardSigmoid : Utility.Activation
     {
+        public override Matrix Forward(Matrix input)
+        {
+            return Matrix.Map(input, Activate);
+        }
+
+        public override Matrix Backward(Matrix input)
+        {
+            return Matrix.Map(input, Derivative);
+        }
+
+        public override double Activate(double input)
+        {
+            if (input < 0)
+            {
+                return 0;
+            }
+            else if (input > 1)
+            {
+                return input;
+            }
+            return input;
+        }
+
+        public override double Derivative(double input)
+        {
+            if (input > 1 || input < 0)
+            {
+                return 0;
+            }
+            return 1;
+        }
+
+        public override Utility.EActivationType Type()
+        {
+            return Utility.EActivationType.HardSigmoid;
+        }
+
+        public override string ToString()
+        {
+            return Type().ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
