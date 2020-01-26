b7d003652356c06e3b1eba4aa4d45a19ed665dc1
Nirex0 <Nirex.0@gmail.com>
2020-01-26 16:23:47 +0330

    Implement: Bipolar Sigmoid Activation class

--- src/XNet/Activation/Core/BipolarSigmoid.cs
+++ src/XNet/Activation/Core/BipolarSigmoid.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using XNet.Math;
 
 namespace XNet.Activation.Core
 {
-    class BipolarSigmoid
+    public sealed class BipolarSigmoid : Utility.Activation
     {
+        public override Matrix Forward(Matrix input)
+        {
+            return Matrix.Map(input, Activate);
+        }
+
+        public override Matrix Backward(Matrix input)
+        {
+            return Matrix.Map(input, Derivative);
+        }
+
+        public override double Activate(double input)
+        {
+            return -1 + 2 / (1 + System.Math.Exp(-input));
+        }
+
+        public override double Derivative(double input)
+        {
+            return 0.5 * (1 + (-1 + 2 / (1 + System.Math.Exp(-input)))) * (1 - (-1 + 2 / (1 + System.Math.Exp(-input))));
+        }
+
+        public override Utility.EActivationType Type()
+        {
+            return Utility.EActivationType.BipolarSigmoid;
+        }
+
+        public override string ToString()
+        {
+            return Type().ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
