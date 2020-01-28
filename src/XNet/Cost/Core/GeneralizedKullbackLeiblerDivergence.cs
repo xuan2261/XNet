8c4688c62fad10e3dd119acf60069278cb79ecf2
Nirex0 <Nirex.0@gmail.com>
2020-01-28 17:43:36 +0330

    Create: Generalized Kullback Leibler Divergence Function class

---
+++ src/XNet/Cost/Core/GeneralizedKullbackLeiblerDivergence.cs
@@ -0,0 +1,57 @@
+﻿// Copyright © 2020 Aryan Mousavi All Rights Reserved.
+
+using System;
+using XNet.Cost.Utility;
+using XNet.XMath;
+
+namespace XNet.Cost.Core
+{
+    public class GeneralizedKullbackLeiblerDivergence : Utility.Cost
+    {
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override Matrix Forward(Matrix Actual, Matrix Expected)
+        {
+            Matrix errorMatrix = Actual.Duplicate();
+            for (int i = 0; i < Actual.rows; i++)
+            {
+                for (int j = 0; j < Actual.cols; j++)
+                {
+                    errorMatrix[i, j] = Expected[i, j] * Math.Log(Expected[i, j] / Actual[i, j]) - Expected[i, j] + Actual[i, j];
+                }
+            }
+            return errorMatrix;
+        }
+
+        public override Matrix Backward(Matrix Actual, Matrix Expected)
+        {
+            Matrix gradMatrix = Actual.Duplicate();
+            for (int i = 0; i < Actual.rows; i++)
+            {
+                for (int j = 0; j < Actual.cols; j++)
+                {
+                    gradMatrix[i, j] = (Actual[i, j] - Expected[i, j]) / Actual[i, j];
+                }
+            }
+            return gradMatrix;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Type().ToString();
+        }
+
+        public override ECostType Type()
+        {
+            return ECostType.GeneralizedKullbackLeiblerDivergence;
+        }
+    }
+}
