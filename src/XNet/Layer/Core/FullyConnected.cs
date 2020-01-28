29a0bb922f042cfe5b1c26e4216c35b86d588008
Nirex <Nirex.0@gmail.com>
2020-01-28 10:38:28 +0330

    Update FullyConnected.cs

--- src/XNet/Layer/Core/FullyConnected.cs
+++ src/XNet/Layer/Core/FullyConnected.cs
@@ -1,12 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using XNet.Layer.Utility;
+using XNet.Math;
+using XNet.Utility;
 
 namespace XNet.Layer.Core
 {
-    class FullyConnected
+    public class FullyConnected : Layer.Utility.Layer
     {
+        public FullyConnected(Activation.Utility.ActivationSettings acs, LayerSettings ls) : base(acs, ls)
+        {
+
+        }
+
+        public override List<List<Matrix>> Backward(List<Matrix> input_data, List<Matrix> input_grad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override List<Matrix> Forward(List<Matrix> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override Dims InShape()
+        {
+            return base.InShape();
+        }
+
+        public override Dims OutShape()
+        {
+            return base.OutShape();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public override ELayerType Type()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
