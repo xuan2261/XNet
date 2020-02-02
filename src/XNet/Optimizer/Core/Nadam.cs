1b45deb5b9ac08865bd5667df49617eb532d40b2
Nirex0 <Nirex.0@gmail.com>
2020-02-02 12:24:00 +0330

    Create: Nadam Placeholder

---
+++ src/XNet/Optimizer/Core/Nadam.cs
@@ -0,0 +1,52 @@
+﻿// Copyright © 2020 Aryan Mousavi All Rights Reserved.
+
+using XNet.XMath;
+using XNet.Optimizer.Utility;
+
+namespace XNet.Optimizer.Core
+{
+    public sealed class Nadam : Utility.Optimizer
+    {
+        public double Alpha { get; set; }
+
+        public Nadam(AdaGradSettings settings) : base(settings)
+        {
+            Alpha = settings.Alpha;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Type().ToString();
+        }
+
+        public override EOptimizerType Type()
+        {
+            return EOptimizerType.Nadam;
+        }
+
+        public override Matrix CalculateDeltaW(Matrix W, Matrix dJdW)
+        {
+            return null;
+        }
+
+        public override Matrix CalculateDeltaB(Matrix b, Matrix dJdb)
+        {
+            return null;
+        }
+    }
+
+    public sealed class NadamSettings : OptimizerSettings
+    {
+        public double Alpha { get; set; }
+    }
+}
