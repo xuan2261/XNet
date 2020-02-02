b66b48fd94612a7afa00387ab6523570a599c62e
Nirex0 <Nirex.0@gmail.com>
2020-02-02 12:24:11 +0330

    Create: Nesterov Momentum Placeholder

--- src/XNet/Optimizer/Core/NesterovMomentum.cs
+++ src/XNet/Optimizer/Core/NesterovMomentum.cs
@@ -1,12 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright © 2020 Aryan Mousavi All Rights Reserved.
+
+using XNet.XMath;
+using XNet.Optimizer.Utility;
 
 namespace XNet.Optimizer.Core
 {
-    class NesterovMomentum
+    public sealed class NesterovMomentum : Utility.Optimizer
     {
+        public double Alpha { get; set; }
+
+        public NesterovMomentum(NesterovMomentumSettings settings) : base(settings)
+        {
+            Alpha = settings.Alpha;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Type().ToString();
+        }
+
+        public override EOptimizerType Type()
+        {
+            return EOptimizerType.NesterovMomentum;
+        }
+
+        public override Matrix CalculateDeltaW(Matrix W, Matrix dJdW)
+        {
+            return null;
+        }
+
+        public override Matrix CalculateDeltaB(Matrix b, Matrix dJdb)
+        {
+            return null;
+        }
+    }
+
+    public sealed class NesterovMomentumSettings : OptimizerSettings
+    {
+        public double Alpha { get; set; }
     }
 }
