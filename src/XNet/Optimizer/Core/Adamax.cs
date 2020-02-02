9ffecbe5134539ed9a8fd7efb23b8696946eab5e
Nirex0 <Nirex.0@gmail.com>
2020-02-02 12:23:20 +0330

    Create: Adamax Placeholder

--- src/XNet/Optimizer/Core/Adamax.cs
+++ src/XNet/Optimizer/Core/Adamax.cs
@@ -1,12 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright © 2020 Aryan Mousavi All Rights Reserved.
+
+using XNet.XMath;
+using XNet.Optimizer.Utility;
 
 namespace XNet.Optimizer.Core
 {
-    class Adamax
+    public sealed class Adamax : Utility.Optimizer
     {
+        public double Alpha { get; set; }
+
+        public Adamax(AdamaxSettings settings) : base(settings)
+        {
+            Alpha = settings.Alpha;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Type().ToString();
+        }
+
+        public override EOptimizerType Type()
+        {
+            return EOptimizerType.Adamax;
+        }
+
+        public override Matrix CalculateDeltaW(Matrix W, Matrix dJdW)
+        {
+            return null;
+        }
+
+        public override Matrix CalculateDeltaB(Matrix b, Matrix dJdb)
+        {
+            return null;
+        }
+    }
+
+    public sealed class AdamaxSettings : OptimizerSettings
+    {
+        public double Alpha { get; set; }
+        public double BetaPrimary { get; set; }
+        public double BetaSecondary { get; set; }
     }
 }
