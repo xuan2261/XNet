83f6669b0e481182c7cd058f70130dbf6a78d41f
Nirex0 <Nirex.0@gmail.com>
2020-02-02 11:34:05 +0330

    Add: Regularization to Cost function base class

--- src/XNet/Cost/Utility/Cost.cs
+++ src/XNet/Cost/Utility/Cost.cs
@@ -1,12 +1,44 @@
 ﻿// Copyright © 2020 Aryan Mousavi All Rights Reserved.
 
+using XNet.Regularization.Utility;
 using XNet.XMath;
 
 namespace XNet.Cost.Utility
 {
+    /// <summary>
+    /// A cost function is a measure of "how good" a neural network did with respect to it's given training sample and the expected output. It also may depend on variables such as weights and biases.
+    /// A cost function is a single value, not a vector, because it rates how good the neural network did as a whole.
+    /// </summary>
     public abstract class Cost
     {
-        public abstract Matrix Forward(Matrix Actual, Matrix Expected);
+        // Second param might be changed to "RegularizationSettings" type
+        public Cost(ERegularizationType regularizationType, double Lambda)
+        {
+            RegularizationType = regularizationType;
+
+            switch (RegularizationType)
+            {
+                case ERegularizationType.Invalid:
+                    throw new System.ArgumentException("Invalid Regularization Type selected, Use None instead.");
+                case ERegularizationType.None:
+                    Regularization = new Regularization.Core.None();
+                    break;
+                case ERegularizationType.L1:
+                    Regularization = new Regularization.Core.L1(Lambda);
+                    break;
+                case ERegularizationType.L2:
+                    Regularization = new Regularization.Core.L2(Lambda);
+                    break;
+                default:
+                    throw new System.ArgumentException("Invalid Regularization Type selected, Use None instead.");
+            }
+        }
+
+        public Regularization.Utility.Regularization Regularization;
+
+        public ERegularizationType RegularizationType { get; private set; }
+
+        public abstract double Forward(Matrix Actual, Matrix Expected);
 
         public abstract Matrix Backward(Matrix Actual, Matrix Expected);
 
