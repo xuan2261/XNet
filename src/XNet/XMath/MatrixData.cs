469ae24f2313eb59ee2469ec1acfe35622b21c30
Nirex0 <Nirex.0@gmail.com>
2020-02-02 15:53:20 +0330

    Update MatrixData.cs

--- src/XNet/XMath/MatrixData.cs
+++ src/XNet/XMath/MatrixData.cs
@@ -1,12 +1,11 @@
-﻿using System.Collections.Generic;
+﻿// Copyright © 2020 Aryan Mousavi All Rights Reserved.
 
-
+using System.Collections.Generic;
 
 namespace XNet.XMath
 {
     public class MatrixData
     {
-
         public Dictionary<string, Matrix> Data { get; private set; }
 
         public MatrixData()
