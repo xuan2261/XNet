913cd9d4f69ee8ea0c73f104e65a49bef81ebdb0
Nirex0 <Nirex.0@gmail.com>
2020-02-02 12:18:04 +0330

    Add: OptimizerSettings class

--- src/XNet/Optimizer/Utility/Optimizer.cs
+++ src/XNet/Optimizer/Utility/Optimizer.cs
@@ -1,11 +1,18 @@
-﻿using XNet.XMath;
+﻿// Copyright © 2020 Aryan Mousavi All Rights Reserved.
+
+using XNet.XMath;
 
 namespace XNet.Optimizer.Utility
 {
     public abstract class Optimizer
     {
+        public Optimizer(OptimizerSettings settings) {}
         public abstract Matrix CalculateDeltaW(Matrix W, Matrix dJdW);
         public abstract Matrix CalculateDeltaB(Matrix b, Matrix dJdb);
         public abstract EOptimizerType Type();
+    }
+
+    public abstract class OptimizerSettings
+    {
     }
 }
