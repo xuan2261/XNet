6691985ed2d58d06beb1aa930c2a28eb21beaba4
Nirex0 <Nirex.0@gmail.com>
2020-02-02 11:32:51 +0330

    Fix: Cross Entropy Cost function class

--- src/XNet/Cost/Core/CrossEntropyCost.cs
+++ src/XNet/Cost/Core/CrossEntropyCost.cs
@@ -2,28 +2,40 @@
 
 using System;
 using XNet.Cost.Utility;
+using XNet.Regularization.Utility;
 using XNet.XMath;
 
 namespace XNet.Cost.Core
 {
+    /// <summary>
+    /// "Cross Entropy Cost": Also known as "Bernoulli negative log-likelihood" and "Binary Cross-Entropy"
+    /// </summary>
     public class CrossEntropyCost : Utility.Cost
     {
+        public CrossEntropyCost(ERegularizationType regularizationType, double Lambda) : base(regularizationType, Lambda) { }
+
         public override bool Equals(object obj)
         {
             return base.Equals(obj);
         }
 
-        public override Matrix Forward(Matrix Actual, Matrix Expected)
+        public override double Forward(Matrix Actual, Matrix Expected)
         {
-            Matrix errorMatrix = Actual.Duplicate();
+            double error = 0.0;
+            if (Actual.rows != Expected.rows || Actual.cols != Expected.cols)
+            {
+                throw new MatrixException("Actual does not have the same size as Expected");
+            }
+
             for (int i = 0; i < Actual.rows; i++)
             {
                 for (int j = 0; j < Actual.cols; j++)
                 {
-                    errorMatrix[i, j] = -Expected[i, j] * Math.Log(Actual[i, j]) + (1.0 - Expected[i, j]) * Math.Log(1 - Actual[i, j]);
+                    error += -Expected[i, j] * Math.Log(Actual[i, j]) + (1.0 - Expected[i, j]) * Math.Log(1 - Actual[i, j]);
                 }
             }
-            return errorMatrix;
+
+            return error;
         }
 
         public override Matrix Backward(Matrix Actual, Matrix Expected)
