e4219a7913f63f1cbf287c8d787a034050ecd82e
Nirex0 <Nirex.0@gmail.com>
2020-02-05 14:51:25 +0330

    Update Architecture.cs

--- src/XNet/Utility/Architecture.cs
+++ src/XNet/Utility/Architecture.cs
@@ -33,7 +33,6 @@ namespace XNet.Utility
     /// </summary>
     public class Architecture
     {
-        public XNet.Network.Utility.ENetworkType NetworkType { get; set; }
         public Dims Infrastructure { get; set; }
 
         public Architecture()
@@ -48,115 +47,6 @@ namespace XNet.Utility
         public string Save()
         {
             string tosave = string.Empty;
-
-            tosave = NetworkType.ToString() + Environment.NewLine;
-
-            switch (NetworkType)
-            {
-                case Network.Utility.ENetworkType.Invalid:
-                    {
-                        tosave = "Invalid";
-                        break;
-                    }
-                case Network.Utility.ENetworkType.AE:
-                    {
-                        tosave = "AE";
-                        break;
-                    }
-                case Network.Utility.ENetworkType.DBM:
-                    {
-                        tosave = "DBM";
-                        break;
-                    }
-                case Network.Utility.ENetworkType.DCIGN:
-                    {
-                        tosave = "DCIGN";
-                        break;
-                    }
-                case Network.Utility.ENetworkType.DCN:
-                    {
-                        tosave = "DCN";
-                        break;
-                    }
-                case Network.Utility.ENetworkType.DFFN:
-                    {
-                        tosave = "DFFN";
-                        break;
-                    }
-                case Network.Utility.ENetworkType.DN:
-                    {
-                        tosave = "InvDNalid";
-                        break;
-                    }
-                case Network.Utility.ENetworkType.DRN:
-                    {
-                        tosave = "DRN";
-                        break;
-                    }
-                case Network.Utility.ENetworkType.ELM:
-                    {
-                        tosave = "ELM";
-                        break;
-                    }
-                case Network.Utility.ENetworkType.ESN:
-                    {
-                        tosave = "ESN";
-                        break;
-                    }
-                case Network.Utility.ENetworkType.GAN:
-                    {
-                        tosave = "GAN";
-                        break;
-                    }
-                case Network.Utility.ENetworkType.GRU:
-                    {
-                        tosave = "GRU";
-                        break;
-                    }
-                case Network.Utility.ENetworkType.KN:
-                    {
-                        tosave = "KN";
-                        break;
-                    }
-                case Network.Utility.ENetworkType.LSM:
-                    {
-                        tosave = "LSM";
-                        break;
-                    }
-                case Network.Utility.ENetworkType.LSTM:
-                    {
-                        tosave = "LSTM";
-                        break;
-                    }
-                case Network.Utility.ENetworkType.NTM:
-                    {
-                        tosave = "NTM";
-                        break;
-                    }
-                case Network.Utility.ENetworkType.RBM:
-                    {
-                        tosave = "RBM";
-                        break;
-                    }
-                case Network.Utility.ENetworkType.RNN:
-                    {
-                        tosave = "RNN";
-                        break;
-                    }
-                case Network.Utility.ENetworkType.SVM:
-                    {
-                        tosave = "SVM";
-                        break;
-                    }
-                default:
-                    {
-                        tosave = "Invalid";
-                        break;
-                    }
-            }
-
-            tosave += Environment.NewLine;
-
             foreach (int item in Infrastructure.Values)
             {
                 tosave += item.ToString() + Environment.NewLine;
@@ -178,83 +68,6 @@ namespace XNet.Utility
                 throw new InvalidArchitecture("Architecture does not include network type.");
             }
 
-            if (arch[0] == "AE")
-            {
-                NetworkType = Network.Utility.ENetworkType.AE;
-            }
-            else if (arch[0] == "DBM")
-            {
-                NetworkType = Network.Utility.ENetworkType.DBM;
-            }
-            else if (arch[0] == "DCIGN")
-            {
-                NetworkType = Network.Utility.ENetworkType.DCIGN;
-            }
-            else if (arch[0] == "DCN")
-            {
-                NetworkType = Network.Utility.ENetworkType.DCN;
-            }
-            else if (arch[0] == "DFFN")
-            {
-                NetworkType = Network.Utility.ENetworkType.DFFN;
-            }
-            else if (arch[0] == "DN")
-            {
-                NetworkType = Network.Utility.ENetworkType.DN;
-            }
-            else if (arch[0] == "DRN")
-            {
-                NetworkType = Network.Utility.ENetworkType.DRN;
-            }
-            else if (arch[0] == "ELM")
-            {
-                NetworkType = Network.Utility.ENetworkType.ELM;
-            }
-            else if (arch[0] == "ESN")
-            {
-                NetworkType = Network.Utility.ENetworkType.ESN;
-            }
-            else if (arch[0] == "GAN")
-            {
-                NetworkType = Network.Utility.ENetworkType.GAN;
-            }
-            else if (arch[0] == "GRU")
-            {
-                NetworkType = Network.Utility.ENetworkType.GRU;
-            }
-            else if (arch[0] == "KN")
-            {
-                NetworkType = Network.Utility.ENetworkType.KN;
-            }
-            else if (arch[0] == "LSM")
-            {
-                NetworkType = Network.Utility.ENetworkType.LSM;
-            }
-            else if (arch[0] == "LSTM")
-            {
-                NetworkType = Network.Utility.ENetworkType.LSTM;
-            }
-            else if (arch[0] == "NTM")
-            {
-                NetworkType = Network.Utility.ENetworkType.NTM;
-            }
-            else if (arch[0] == "RBM")
-            {
-                NetworkType = Network.Utility.ENetworkType.RBM;
-            }
-            else if (arch[0] == "RNN")
-            {
-                NetworkType = Network.Utility.ENetworkType.RNN;
-            }
-            else if (arch[0] == "SVM")
-            {
-                NetworkType = Network.Utility.ENetworkType.SVM;
-            }
-            else
-            {
-                NetworkType = Network.Utility.ENetworkType.Invalid;
-            }
-
             Infrastructure.Values.Clear();
 
             foreach (string item in arch)
@@ -265,7 +78,7 @@ namespace XNet.Utility
                 }
                 else
                 {
-                    throw new ArgumentException();
+                    throw new ArgumentException("Could not parse string to int.");
                 }
             }
         }
