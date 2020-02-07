9ee7a0892d617598347203cc030abc937650c88d
Nirex <Nirex.0@gmail.com>
2020-02-07 14:33:13 +0330

    Update: Core Layers' foundation is done

--- src/XNet/Layer/Core/LSTM.cs
+++ src/XNet/Layer/Core/LSTM.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright © 2020 Aryan Mousavi All Rights Reserved.
+
+using System;
+using XNet.Layer.Utility;
+using XNet.XMath;
 
 namespace XNet.Layer.Core
 {
-    class LSTM
+    public class LSTM : Layer.Utility.Layer
     {
+        public int NeuronCount { get; private set; }
+
+        public LSTM(int neuronCount, int index, Activation.Utility.ActivationSettings activationSettings) : base(neuronCount, index, activationSettings)
+        {
+            NeuronCount = neuronCount;
+        }
+
+        public override void Backward(ref MatrixData input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Forward(ref MatrixData input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ELayerType Type()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- src/XNet/Layer/Core/Recurrent.cs
+++ src/XNet/Layer/Core/Recurrent.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright © 2020 Aryan Mousavi All Rights Reserved.
+
+using System;
+using XNet.Layer.Utility;
+using XNet.XMath;
 
 namespace XNet.Layer.Core
 {
-    class Recurrent
+    public class Recurrent : Layer.Utility.Layer
     {
+        public int NeuronCount { get; private set; }
+
+        public Recurrent(int neuronCount, int index, Activation.Utility.ActivationSettings activationSettings) : base(neuronCount, index, activationSettings)
+        {
+            NeuronCount = neuronCount;
+        }
+
+        public override void Backward(ref MatrixData input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Forward(ref MatrixData input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ELayerType Type()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
